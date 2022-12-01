cd1227559e93e11db23bcc68d945d833f4aa18bb
Matan Selig <matan.selig@gmail.com>
2022-12-01 03:38:33 +0100

    request courses success

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const debug = require("debug");
+const courses = require("./routes/courses");
 
-const mongodbCollection = "referencing_documents";
+app.use(express.json());
+
+const mongodbCollection = "ReferencingDocuments";
 
 mongoose
   .connect(`mongodb://localhost/${mongodbCollection}`)
-  .then(() => console.log(f`connect to mongodb ${mongodbCollection}`))
+  .then(() => console.log(`Connect to Mongodb ${mongodbCollection}.`))
   .catch((err) => {
     console.log(`ERROR: ${err}`);
   });
+
+app.use("/", courses);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}...`);
+});
