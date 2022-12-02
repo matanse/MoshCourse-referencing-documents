4ae9f7e9337d4de6b600a417e0d2f75675543222
Matan Selig <matan.selig@gmail.com>
2022-12-02 02:54:49 +0100

    add post + debug lines to authors routes

--- routes/authors.js
+++ routes/authors.js
@@ -4,10 +4,18 @@ const netRequest = debug("app:net:Request");
 const netReply = debug("app:net:Reply");
 const { Author, validateAuthor } = require("../models/Author");
 
-router.get("/", async (req, res) => {
+router.get("/api/authors", async (req, res) => {
   const authors = await Author.find();
-  console.log(authors);
+  netReply(authors);
   res.send(authors);
+});
+
+router.post("/api/authors", async (req, res) => {
+  netRequest(req.body);
+  const newAuthor = new Author(req.body);
+  const result = await newAuthor.save();
+  res.send(result);
+  netReply(result);
 });
 
 module.exports = router;
